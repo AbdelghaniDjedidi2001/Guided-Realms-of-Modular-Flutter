0ec0ad50370b443d9ab7ec438df477d1f93f11cb
AbdelghaniDjedidi2001 <abdelghani.djedidi.2022@gmail.com>
2025-07-30 10:53:52 +0100

    add app_setting_provider and it impl

---
+++ packages/features/settings/app_settings/lib/provider/app_setting_provider_impl.dart
@@ -0,0 +1,28 @@
+import 'package:app_settings/provider/app_setting_provider.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: AppSettingProvider, env: [Environment.prod])
+class ProdAppSettingProviderImpl extends AppSettingProvider {
+  @override
+  String getAppLanguge() {
+    return 'En';
+  }
+
+  @override
+  String getThemeType() {
+    return 'dark';
+  }
+}
+
+@Injectable(as: AppSettingProvider, env: [Environment.dev])
+class DevAppSettingProviderImpl extends AppSettingProvider {
+  @override
+  String getAppLanguge() {
+    return 'Ar';
+  }
+
+  @override
+  String getThemeType() {
+    throw UnimplementedError();
+  }
+}
