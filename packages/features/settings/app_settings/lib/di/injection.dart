5cc1ad3059d8487c4f4e5d09b4d80ca840a9c9fe
AbdelghaniDjedidi2001 <abdelghani.djedidi.2022@gmail.com>
2025-07-30 11:13:44 +0100

    test DI with env dev | prod

--- packages/features/settings/app_settings/lib/di/injection.dart
+++ packages/features/settings/app_settings/lib/di/injection.dart
@@ -1,7 +1,15 @@
+import 'package:app_settings/provider/app_setting_provider.dart';
+import 'package:app_settings/provider/app_setting_provider_impl.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'injection.config.dart';
 
-
 @InjectableInit()
-void configureAppSettingsDependency(GetIt getIt, String? environment) => getIt.init(environment: environment);
+void configureAppSettingsDependency(GetIt getIt, String? environment) {
+  getIt.init(environment: environment);
+  getIt.registerSingleton<AppSettingProvider>(
+    environment == Environment.prod
+        ? ProdAppSettingProviderImpl()
+        : DevAppSettingProviderImpl(),
+  );
+}
